3bf81fdb46fd1cd3c5a10faf958572edef427195
Nils Steinbiß <nils.steinbiss@appsfactory.de>
2023-06-15 11:58:09 +0200

    initial commit

---
+++ automotive/src/main/java/de/appsfactory/testmediaservice/TestActivity.kt
@@ -0,0 +1,14 @@
+package de.appsfactory.testmediaservice
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.app.ActivityCompat
+
+class TestActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+}
